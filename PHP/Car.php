cfbab36ebb3df826f4468087aba9e9cb5913eff5
Eduardo Flores <edflor46@gmail.com>
2021-07-10 18:20:50 -0500

    instanciar clase PHP

--- PHP/Car.php
+++ PHP/Car.php
@@ -6,4 +6,17 @@ class Car
   public $license;
   public $driver;
   public $passenger;
+
+  public function __construct($license, $driver)
+  {
+    $this->license = $license;
+    $this->driver = $driver;
+  }
+
+  public function printDataCar( )
+  {
+    echo "Licencia: $this->license 
+    Driver: {$this->driver->name}";
+         
+  }
 }
